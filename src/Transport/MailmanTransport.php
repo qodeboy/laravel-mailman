d632b295edff9607143beace4940c8f9703441a0
Pavel Sheiman <pavel.sheiman@gmail.com>
2017-10-03 17:46:42 +0300

    declaration should be compatible Switf_Transport->send()

--- src/Transport/MailmanTransport.php
+++ src/Transport/MailmanTransport.php
@@ -31,7 +31,7 @@ class MailmanTransport extends Transport
     /**
      * {@inheritdoc}
      */
-    public function send(Swift_Mime_Message $message, &$failedRecipients = null)
+    public function send(\Swift_Mime_SimpleMessage $message, &$failedRecipients = null)
     {
         $mailmanMessage = new MailmanMimeMessage($message);
         $this->beforeSendPerformed($message);
